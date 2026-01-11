3108782747a1483b3a9349ce5bbbf5e2de7e2c06
kruben84ec <crmirandazambrano@gmail.com>
2026-01-11 18:17:17 -0500

    Codigo de la EP0

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,10 @@
-import { useEffect, useState } from "react";
-
 function App() {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:8000/products")
-      .then(res => res.json())
-      .then(data => setProducts(data))
-      .catch(err => console.error(err));
-  }, []);
-
   return (
     <div style={{ padding: "2rem", fontFamily: "Arial" }}>
       <h1>Brixo</h1>
-      <p>Control simple de tu inventario</p>
+      <p>Control simple de tu inventario, vs1</p>
 
-      <h2>Productos</h2>
-
-      {products.length === 0 && <p>No hay productos</p>}
-
-      <ul>
-        {products.map(p => (
-          <li key={p.id}>
-            {p.name} — Stock: {p.stock}
-          </li>
-        ))}
-      </ul>
+      <p>Frontend activo.{{}}</p>
     </div>
   );
 }
