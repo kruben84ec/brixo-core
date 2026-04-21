03233e2deb648cc38da36221a42ba3f32ce5a2a3
kruben84ec <crmirandazambrano@gmail.com>
2026-04-21 03:05:52 -0500

    Revert "step 4: tarea 2 completada - design tokens, ThemeProvider y useTheme hook"
    This reverts commit 276eb77e9af4ddf3fa4da8216c62616d6adb2b85.

--- frontend/src/theme/useTheme.ts
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react';
-import { ThemeContext } from './ThemeProvider';
-
-export function useTheme() {
-  const ctx = useContext(ThemeContext);
-  if (!ctx) throw new Error('useTheme debe usarse dentro de <ThemeProvider>');
-  return ctx;
-}
